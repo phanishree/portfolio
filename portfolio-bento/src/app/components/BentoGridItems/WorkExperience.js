785106e95a5e3e0ac6740020b29f8d45395a3ba2
phani <phanis18191@gmail.com>
2025-01-07 17:32:20 +0530

    Add route to work experience item

--- portfolio-bento/src/app/components/BentoGridItems/WorkExperience.js
+++ portfolio-bento/src/app/components/BentoGridItems/WorkExperience.js
@@ -1,26 +1,29 @@
 "use client"
 import React from "react";
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import Link from 'next/link';
 
 export default function WorkExperience() {
     return (
-      <div className="w-full h-full text-white  bg-darkNavy rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4 p-4 transform hover:scale-101 hover:shadow-lg transition duration-300 cursor-pointer">
-        {/* Title */}
-        <p className="text-2xl font-bold">Work Experience</p>
-  
-        {/* Lottie Animation */}
-        <DotLottieReact
-          src="https://lottie.host/78245a4d-470a-4f52-b055-23ea3330bad2/D4PrnIXrbg.lottie"
-          loop
-          autoplay
-          className="w-48 h-48"
-        />
-  
-        {/* Work Info */}
-        <section className="text-center">
-          <p className="text-lg font-medium">Currently working at:</p>
-          <p className="text-xl font-semibold">Synamedia</p>
-        </section>
-      </div>
+      <Link href="/work" className="w-full h-full flex items-center justify-center">
+        <div className="w-full h-full text-white  bg-darkNavy rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4 p-4 transform hover:scale-101 hover:shadow-lg transition duration-300 cursor-pointer">
+          {/* Title */}
+          <p className="text-2xl font-bold">Work Experience</p>
+    
+          {/* Lottie Animation */}
+          <DotLottieReact
+            src="https://lottie.host/78245a4d-470a-4f52-b055-23ea3330bad2/D4PrnIXrbg.lottie"
+            loop
+            autoplay
+            className="w-48 h-48"
+          />
+    
+          {/* Work Info */}
+          <section className="text-center">
+            <p className="text-lg font-medium">Currently working at:</p>
+            <p className="text-xl font-semibold">Synamedia</p>
+          </section>
+        </div>
+      </Link>
     );
   }
---
+++ portfolio-bento/src/app/work/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function WorkExperience() {
+    return (
+      <div className="w-[100vw] h-[100vh] flex items-center justify-center text-white  bg-darkNavy">
+        <h1>Work Experience</h1>
+        <p>Details about your work experience go here.</p>
+      </div>
+    );
+  }
+  
