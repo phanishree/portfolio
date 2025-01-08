cf5c2143ec05115aa49cccc41b25cab47c5aa0d6
phani <phanis18191@gmail.com>
2025-01-08 22:29:21 +0530

    Refactor

--- portfolio-bento/src/app/work/page.js
+++ portfolio-bento/src/app/work/page.js
@@ -4,7 +4,7 @@ export default function WorkExperience() {
     return (
       <div className="w-[100vw] h-[100vh] flex items-center justify-center text-white  bg-darkNavy">
         <h1>Work Experience</h1>
-        <p>Details about your work experience go here.</p>
+        <p>Cook fast</p>
       </div>
     );
   }
